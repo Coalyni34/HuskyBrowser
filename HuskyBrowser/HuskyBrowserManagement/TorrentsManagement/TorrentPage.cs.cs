7aab038767fd75edd78d70af02058ad9f79d481b
Coalyni <deltacandy2@gmail.com>
2024-11-21 15:58:04 +0300

    Little changes

---
+++ HuskyBrowser/HuskyBrowserManagement/TorrentsManagement/TorrentPage.cs.cs
@@ -0,0 +1,14 @@
+﻿using HuskyBrowser.WorkingWithBrowserProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuskyBrowser.HuskyBrowserManagement.TorManager
+{
+    public class TorrentPage : PagePattern
+    {
+
+    }
+}
