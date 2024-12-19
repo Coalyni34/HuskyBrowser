e0e7a12752c95fbc1bdd3fdffb45d70d008ff739
Coalyni <deltacandy2@gmail.com>
2024-12-19 22:00:22 +0300

    Just added a TOR folder

---
+++ HuskyBrowser/HuskyBrowserManagement/BrowserManagement/SettingsManagement/settingsmanagement/Settings.cs
@@ -0,0 +1,28 @@
+﻿using MaterialSkin;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace HuskyBrowser.WorkingWithBrowserProperties
+{
+    public class Settings
+    {             
+        public string Enabled_Search_Engine { get; set; }
+        public string Start_Page { get; set; }
+        public bool Save_History { get; set; }
+        public string Search_Engine_Name { get; set; }
+        public int[] ScreenResolution { get; set; }
+        public string SaveDirectoryPath { get; set; }
+        public string TypeOfStartPage { get; set; }
+        public Settings(string enabled_search_engine, string start_page, bool save_history, string search_Engine_Name, int[] screenResolution, string saveDirectoryPath, string typeOfStartPage)
+        {
+            Enabled_Search_Engine = enabled_search_engine;
+            Start_Page = start_page;
+            Save_History = save_history;
+            Search_Engine_Name = search_Engine_Name;
+            ScreenResolution = screenResolution;
+            SaveDirectoryPath = saveDirectoryPath;
+            TypeOfStartPage = typeOfStartPage;
+        }           
+    }
+}
